ee3bfc233b8026d6e665e7cd6477efeb5ea020de
Jesse <jessetyoungblood@gmail.com>
2019-12-19 12:26:01 -0500

    Optionally set user execution policy
    This part is optional, but it seems like a common problem.
    
    After installing pm2, invoking it on the command line on a machine with an ExecutionPolicy set to Restricted will fail, even if you run Unblock-File beforehand.
    
    This script checks the current user's ExecutionPolicy and, if it's Restricted or Undefined, offers to change it to RemoteSigned.

---
+++ src/windows/configure-policy-shell.ps1
@@ -0,0 +1,13 @@
+param(
+	[string] $Force = $true
+)
+
+# & .\src\windows\configure-policy.ps1
+
+# Instead of calling the following script, unlock the script file, then call it directly with the same execution policy it might be setting
+# This is entirely to supress the otherwise incredibly bad-looking notice that will pop up if you change an execution policy from a script executing from within a context that doesn't share that policy
+# E.g. if this script is invoked with -ExecutionPolicy Bypass, and the user opts to alter their user-level execution policy, it'll throw a large error-looking message of red text even if it works
+# "Windows PowerShell updated your execution policy successfully, but the setting is overridden by a policy defined at a more specific scope."
+$script_path = ".\src\windows\configure-policy.ps1"
+Unblock-File -Path $script_path
+powershell.exe -NoLogo -ExecutionPolicy RemoteSigned -Command "$script_path -Force $Force"
---
+++ src/windows/configure-policy.ps1
@@ -0,0 +1,36 @@
+param(
+	[string] $Force = $false
+)
+
+Write-Host "=== Checking PowerShell Execution Policy ==="
+
+$policy = Get-ExecutionPolicy -Scope CurrentUser
+
+if ($policy -ne "Restricted" -and $policy -ne "Undefined") {
+  Write-Host "The current user's execution policy appears valid: $policy"
+  Write-Output $true
+  exit
+}
+
+Write-Host "The current user's execution policy is `"$policy`", which may interfere with your ability to interface with pm2."
+
+$preferred = "RemoteSigned"
+
+if ($Force -eq $false) {
+
+  $confirmation = Read-Host -Prompt "Would you like to update it to `"$preferred`"? (Y/N)"
+
+  while ($confirmation -ne "y" -and $confirmation -ne "Y") {
+    
+    if ($confirmation -eq 'n' -or $confirmation -eq 'N') {
+      Write-Output $false
+      exit
+    }
+
+     $confirmation = Read-Host -Prompt "(Y/N)"
+  }
+}
+
+Write-Host "Updating current user's execution policy to $preferred..."
+Set-ExecutionPolicy $preferred -Scope CurrentUser -Force -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+Write-Host "Complete."
