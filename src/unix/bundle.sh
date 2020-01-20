9cd9d87d454cec3748c2c3341c1cd59632183aac
Jesse <jessetyoungblood@gmail.com>
2020-01-19 19:02:03 -0500

    Update bundle commands
    Install using --global-style, but locally, as to not interfere with the build machine's global packages.

--- src/unix/bundle.sh
+++ src/unix/bundle.sh
@@ -10,9 +10,12 @@ cache_folder="./.npm_cache";
 rm -rf $cache_folder
 mkdir -p $cache_folder
 
-echo 'Populating cache with all global dependencies..'
-npm install --global --force --cache $cache_folder --shrinkwrap false --loglevel=error $pm2_package
-npm install --global --force --cache $cache_folder --shrinkwrap false --loglevel=error $pm2_logrotate_package
+echo 'Populating cache with all dependencies..'
+npm install --global-style --force --cache $cache_folder --loglevel=error $pm2_package
+npm install --global-style --force --cache $cache_folder --loglevel=error $pm2_logrotate_package
+
+echo 'Removing local npm_modules folder..'
+rm -rf node_modules
 
 # Write system information to bundle file
 node src/bundle/write.js
--- src/windows/bundle.ps1
+++ src/windows/bundle.ps1
@@ -4,6 +4,7 @@ $pm2_package = "$(node src/echo-dependency.js pm2)"
 $pm2_service_package = "$(node src/echo-dependency.js pm2-windows-service)";
 $pm2_logrotate_package = "$(node src/echo-dependency.js pm2-logrotate)"
 $cache_folder = ".\.npm_cache";
+$node_modules = ".\node_modules";
 
 # Remove the existing npm cache, if one exists
 if (Test-Path $cache_folder) {
@@ -14,15 +15,18 @@ if (Test-Path $cache_folder) {
 # Create a new npm cache folder
 New-Item -ItemType Directory -Name $cache_folder | Out-Null
 
-Write-Host "Temporarily uninstalling global dependencies.."
-npm uninstall --global --loglevel=error "pm2"
-npm uninstall --global --loglevel=error "@innomizetech/pm2-windows-service"
-npm uninstall --global --loglevel=error "pm2-logrotate"
+Write-Host "Temporarily uninstalling dependencies.."
+npm uninstall --global-style --loglevel=error "pm2"
+npm uninstall --global-style --loglevel=error "@innomizetech/pm2-windows-service"
+npm uninstall --global-style --loglevel=error "pm2-logrotate"
 
-Write-Host "Populating cache with all global dependencies.."
-npm install --global --force --cache $cache_folder --shrinkwrap false --loglevel=error $pm2_package
-npm install --global --force --cache $cache_folder --shrinkwrap false --loglevel=error $pm2_service_package
-npm install --global --force --cache $cache_folder --shrinkwrap false --loglevel=error $pm2_logrotate_package
+Write-Host "Populating cache with all dependencies.."
+npm install --global-style --force --cache $cache_folder --shrinkwrap false --loglevel=error $pm2_package
+npm install --global-style --force --cache $cache_folder --shrinkwrap false --loglevel=error $pm2_service_package
+npm install --global-style --force --cache $cache_folder --shrinkwrap false --loglevel=error $pm2_logrotate_package
+
+Write-Host "Removing local npm_modules folder.."
+Remove-Item $node_modules -recurse | Out-Null
 
 # Write system information to bundle file
 node src\bundle\write.js
