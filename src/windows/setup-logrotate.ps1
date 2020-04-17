3a7e985f76fa5350417094b34b674a343f82be8f
Jesse <jessetyoungblood@gmail.com>
2020-04-17 13:06:51 -0400

    Update Windows logrotate installation script
    Ensure pm2 remains --silent

--- src/windows/setup-logrotate.ps1
+++ src/windows/setup-logrotate.ps1
@@ -8,7 +8,7 @@ $logrotate_directory = "$(npm config get prefix)\node_modules\pm2-logrotate\"
 Write-Host "Using directory: $logrotate_directory"
 
 cd $logrotate_directory
-pm2 install .
+pm2 install . --silent
 pm2 save --force
 
 # Go back to where we came from
