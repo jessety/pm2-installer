09a0751bf8db87700058a38c49e4873ff54c330c
Jesse <jessetyoungblood@gmail.com>
2020-05-04 10:34:58 -0400

    Improve Windows logrotate removal process
    Confirm `pm2` is still installed before attempting to invoke it

--- src/windows/remove-logrotate.ps1
+++ src/windows/remove-logrotate.ps1
@@ -1,9 +1,16 @@
 Write-Host "=== Remove Log Rotation ==="
 
-# Delete the running module
-pm2 delete pm2-logrotate --silent
+if (Get-Command "pm2" -ErrorAction SilentlyContinue) {
 
-# Uninstall the module
-pm2 uninstall pm2-logrotate --silent 
+  # Delete the running module
+  pm2 delete pm2-logrotate --silent
+
+  # Uninstall the module
+  pm2 uninstall pm2-logrotate --silent
+
+} else {
+
+  Write-Host "Command `"pm2`" not found, potentially because it has already been uninstalled. Not removing log rotation, since pm2 itself has alrady been removed."
+}
 
 Write-Host "=== Remove Log Rotation Complete ==="
